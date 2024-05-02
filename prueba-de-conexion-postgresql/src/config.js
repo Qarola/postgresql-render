6e85e26a71cd9f77cf69579f6a1b557837676581
Qarola <cyan.323@gmail.com>
2024-05-02 19:17:15 -0300

    update config

--- prueba-de-conexion-postgresql/src/config.js
+++ prueba-de-conexion-postgresql/src/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'https://postgresql-render.onrender.com',  /* process.env.DB_HOST, */
+  host: 'postgresql-render.onrender.com',  /* process.env.DB_HOST, */
   dialect: 'postgres'
 });
 
