7e78966c1ec3e40a51039be296578361d9267c0a
Qarola <cyan.323@gmail.com>
2024-05-02 22:09:11 -0300

    modify config to deploy in Render

--- prueba-de-conexion-postgresql/src/config.js
+++ prueba-de-conexion-postgresql/src/config.js
@@ -1,9 +1,24 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'postgresql-render.onrender.com',  /* process.env.DB_HOST, */
-  dialect: 'postgres'
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    connectTimeout: 30000,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
+
+/* const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'postgresql-render.onrender.com',  /* process.env.DB_HOST, 
+  dialect: 'postgres',
+  dialectOptions: {
+    connectTimeout: 30000 
+  }
+}); */
 
 module.exports = sequelize;
